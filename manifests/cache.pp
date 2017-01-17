804bbc3a8be1252689f6449f91902daaea3db194
Flannon Jackson <flannon@nyu.edu>
2017-01-17 15:37:22 -0500

    Load the cache maintenance script to a cron job.

---
+++ manifests/cache.pp
@@ -0,0 +1,44 @@
+# == Class: loris::cache
+#
+# Manage the loris image cache maintenance script
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# === Variables
+#
+# === Examples
+#
+#  class { 'loris':
+#  }
+#
+# === Authors
+#
+# Flannon Jackson <flannon@nyu.edu>
+#
+# === Copyright
+#
+# Copyright 2017 Your name here, unless otherwise noted.
+#
+class loris::cache(
+  $user_home = $loris::params::user_home,
+  ) inherits loris::params{
+
+    file { '/var/log/loris2' :
+      ensure => directory,
+      owner  => $user,
+      group  => $user,
+      mode   => '0755',
+    }
+    # Clean the loris cache once a day at midnight.
+    # This is a preliminary value and will need to be adjusted
+    # once the server is running in production.
+    cron { 'loris-cache_clean.sh':
+      ensure  => present,
+      command => "${user_home}/setup/loris2/bin/",
+      user    => $user,
+      hour    => '0',
+      minute  => '5',
+    }
+}
