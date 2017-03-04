02831d46caa7f40738420cba18edf4bce5f77d4b
Flannon Jackson <flannon@nyu.edu>
2017-03-04 12:25:52 -0500

    Reactored to include all the loris install options

--- manifests/install/redhat7.pp
+++ manifests/install/redhat7.pp
@@ -1,19 +1,34 @@
 # == Class: loris
 #
 class loris::install::redhat7(
-  #$image_dir = heira('loris::image_dir', $loris::params::image_dir), 
-  #$user      = heira('loris::user', $loris::params::user), 
-  $default_vhost = $loris::params::default_vhost,
-  $image_dir = $loris::params::image_dir,
-  $user      = $loris::params::user,
-  $user_home = $loris::params::user_home,
+  #$image_dir = hiera('loris::image_dir', $loris::params::image_dir), 
+  #$user      = hiera('loris::user', $loris::params::user), 
+  $confdir          = hiera('loris::confdir', $loris::params::confdir),
+  $default_vhost    = hiera('loris::default_vhost', $loris::params::default_vhost),
+  $image_cache      = hiera('loris::image_cache', $loris::params::image_cache),
+  $image_dir        = $loris::params::image_dir,
+  $info_cache       = hiera('loris::info_cache', $loris::params::info_cache),
+  $kdu_expand       = hiera('loris::kdu_expand', $loris::params::kdu_expand),
+  $libkdu           = hiera('loris::libkdu', $loris::params::libkdu),
+  $log_dir          = hiera('loris::log_dir', $loris::params::log_dir),
+  $loris_group      = hiera('loris::loris_group', $loris::params::loris_group),
+  $loris_owner      = hiera('loris::loris_ownder', $loris::params::loris_owner),
+  $loris_owner_home = hiera('loris::loris_owner_home', $loris::params::loris_owner_home),
+  $loris_revision   = hiera('loris::loris_revision', $loris::params::loris_revision),
+  $server_name      = hiera('loris::server_name', $loris::params::server_name),
+  $server_admin     = hiera('loris::server_admin', $loris::params::server_admin),
+  $source_images    = hiera('loris::source_image', $loris::params::source_images),
+  $tmp_dir          = hiera('loris::tmp_dir', $loris::params::tmp_dir),
+  $www_dir          = hiera('loris::www_dir', $loris::params::www_dir),
+  $user             = $loris::params::user,
+  $user_home        = $loris::params::user_home,
 
 ) inherits loris::params {
 
   # Make the loris user
-  user { $user :
+  user { $loris_owner :
     ensure => present,
-    home   => $user_home,
+    home   => $loris_owner_home,
     shell  => '/bin/bash',
   }
   # Eventually loris will be dependent on apache
@@ -21,13 +36,13 @@ class loris::install::redhat7(
   file { '/var/www':
     ensure => directory,
   }
-  file { $user_home:
+  file { $www_dir:
     ensure => directory,
     owner  => $user,
     group  => $user,
     mode   => '0755',
   }
-  file { $image_dir:
+  file { $source_images:
     ensure => directory,
     owner  => $user,
     group  => $user,
@@ -76,49 +91,45 @@ class loris::install::redhat7(
   #  owner      => 'root',
   #  timeout    => 1800,
   #}
-  python::virtualenv { "${user_home}/virtualenv" :
+  python::virtualenv { "${www_dir}/virtualenv" :
     ensure     => present,
     version    => 'system',
     systempkgs => true,
     #distribute => false,
-    venv_dir   => "${user_home}/virtualenv",
-    owner      => $user,
-    group      => $user,
-    #owner      => 'root',
-    #group      => 'root',
-    #cwd        => '/tmp',
+    venv_dir   => "${www_dir}/virtualenv",
+    owner      => $loris_owner,
+    group      => $loris_group,
     timeout    => 0,
     require    => Package['python-virtualenv.noarch'],
   }
-  python::pip { "${user_home}/virtualenv Werkzeug":
+  python::pip { "${www_dir}/virtualenv Werkzeug":
     ensure     => present,
     pkgname    => 'Werkzeug',
-    virtualenv => "${user_home}/virtualenv",
+    virtualenv => "${www_dir}/virtualenv",
     owner      => 'loris',
     #owner      => 'root',
     timeout    => 1800,
   }
-  python::pip { "${user_home}/virtualenv Pillow":
+  python::pip { "${www_dir}/virtualenv Pillow":
     ensure     => present,
     pkgname    => 'Pillow',
-    virtualenv => "${user_home}/virtualenv",
-    owner      => 'loris',
-    #owner      => 'root',
+    virtualenv => "${www_dir}/virtualenv",
+    owner      => $loris_owner,
     timeout    => 1800,
   }
-  file { "${user_home}/setup":
+  file { "${www_dir}/src":
     ensure => directory,
-    owner  => $user,
-    group  => $user,
+    owner  => $loris_owner,
+    group  => $loris_group,
     mode   => '0755',
   }
-  file { "${user_home}/setup/loris2":
+  file { "${www_dir}/src/loris2":
     ensure => directory,
-    owner  => $user,
-    group  => $user,
+    owner  => $loris_owner,
+    group  => $loris_group,
     mode   => '0755',
   }
-  vcsrepo { "${user_home}/setup/loris2" :
+  vcsrepo { "${www_dir}/src/loris2" :
     ensure   => present,
     provider => git,
     source   => 'git://github.com/loris-imageserver/loris',
@@ -127,38 +138,38 @@ class loris::install::redhat7(
 
   exec { 'exec_loris_setup' :
     path    => [ '/bin','/usr/bin', '/usr/local/bin', 
-                "${user_home}/virtualenv/bin"],
+                "${www_dir}/virtualenv/bin"],
     #path    => [ '/bin', '/usr/bin', '/usr/local/bin'],
-    cwd     => "${user_home}/setup/loris2",
-    command => "${user_home}/virtualenv/bin/python setup.py install",
+    cwd     => "${www_dir}/setup/loris2",
+    command => "${www_dir}/virtualenv/bin/python setup.py install --source-images $source_images --image-cache $image_cache",
     #command => "python setup.py install",
-    creates => "${user_home}/loris2.wsgi",
-    require => [ Class['apache'], Vcsrepo["${user_home}/setup/loris2"],
-                 Python::Virtualenv[ "${user_home}/virtualenv"] ], 
-    notify    => File["${user_home}/loris2.wsgi"],
+    creates => "${www_dir}/loris2.wsgi",
+    require => [ Class['apache'], Vcsrepo["${www_dir}/setup/loris2"],
+                 Python::Virtualenv[ "${www_dir}/virtualenv"] ], 
+    notify    => File["${www_dir}/loris2.wsgi"],
     tries     => '5',
     try_sleep => '1',
     user      => 'root',
   }
-  file { "${user_home}/loris2.wsgi" :
+  file { "${www_dir}/loris2.wsgi" :
     ensure  => present,
-    owner   => $user,
-    group   => $user,
+    owner   => $loris_owner,
+    group   => $loris_group,
     mode    => '0755',
     content => template('loris/loris2.wsgi.erb'),
     require => Exec['exec_loris_setup'],
   }
-  file { '/usr/local/bin/kdu_expand':
+  file { $dku_expand:
     ensure => file,
-    source => "file:///${user_home}/setup/loris2/bin/Linux/x86_64/kdu_expand",
+    source => "file:///${www_dir}/setup/loris2/bin/Linux/x86_64/kdu_expand",
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
     require => Exec['exec_loris_setup'],
   }
-  file { '/usr/local/lib/libkdu_v74R.so':
+  file { "${libkdu}/libkdu_v74R.so":
     ensure => file,
-    source => "file:///${user_home}/setup/loris2/lib/Linux/x86_64/libkdu_v74R.so",
+    source => "file:///${www_dir}/setup/loris2/lib/Linux/x86_64/libkdu_v74R.so",
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
--- manifests/params.pp
+++ manifests/params.pp
@@ -25,11 +25,22 @@ class loris::params {
 
   case $::osfamily {
     'RedHat' : {
+      $config_dir     = '/etc/loris2'
       $default_vhost  = true
+      $image_cache    = '/var/cache/loris2'
       $image_dir      = '/usr/local/share/images'
+      $info_cache     = '/var/cache/loris2'
+      $kdu_expand     = '/usr/local/bin/kdu_expand'
+      $libkdu         = '/usr/local/lib'
+      $log_dir        = '/var/log/loris2'
+      $loris_group    = 'loris'
+      $loris_owner    = 'loris'
       $loris_revision = 'v2.1.0-final'
       $server_name    = 'loris.local'
       $server_admin   = "root@${server_name}"
+      $source_images  = '/usr/local/share/images'
+      $tmp_dir        = '/tmp/loris2'
+      $www_dir        = '/var/www/loris2'
       $user           = 'loris'
       $user_home      = "/var/www/${user}2"
     }
